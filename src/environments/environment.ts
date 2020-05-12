b7aa98e02b8c80fb27ae639b5077b4a642355924
Zerneax <d.chris59@gmail.com>
2020-05-12 21:46:37 +0200

    add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBp-m5am066HroT1SiHuDpT9bE6cXgXbxU",
+    authDomain: "todo-app-53fa8.firebaseapp.com",
+    databaseURL: "https://todo-app-53fa8.firebaseio.com",
+    projectId: "todo-app-53fa8",
+    storageBucket: "todo-app-53fa8.appspot.com",
+    messagingSenderId: "1084692423423",
+    appId: "1:1084692423423:web:86bb1cc2475e0eebefa2ca"
+  }
 };
 
 /*
