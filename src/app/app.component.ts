cccfebfc793d97c875546bd5c9116c97f5a893c9
Zerneax <d.chris59@gmail.com>
2020-05-13 21:48:50 +0200

    gestion ajout modif todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Todo } from './models/todo';
+import { Document } from './models/document';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'todo-app';
+  todosCollection: AngularFirestoreCollection<Todo>;
+  documents: Observable<Document[]>;
+  newTodo: string = '';
+
+  constructor(private firestore: AngularFirestore) {}
+
+  ngOnInit(): void {
+    this.todosCollection = this.firestore.collection<Todo>('todo-list/test/todo');
+
+    this.documents = this.todosCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data: Todo = a.payload.doc.data() as Todo;
+        const id: string = a.payload.doc.id;
+        return {id: id, todo: data};
+      }))
+    );
+  }
+
+  convertTimestampToDate(timestamp: any): Date{
+    return new Date(Number(timestamp.seconds) * 1000);
+  }
+
+  addNewTodo(): void {
+    this.firestore.collection('todo-list/test/todo').add({
+      title: this.newTodo,
+      date: new Date(),
+      completed: false
+    });
+  }
+
+  changeTodoToComplete(doc: Document): void {
+    doc.todo.completed = !doc.todo.completed;
+    this.firestore.doc<Todo>(`todo-list/test/todo/${doc.id}`).update(doc.todo);
+  }
 }
---
+++ src/app/models/todo.ts
@@ -0,0 +1,12 @@
+export interface Todo {
+  title: string;
+  date: Date;
+  completed: boolean;
+
+  // constructor(id_: string, title_: string, date_: Date, completed_: boolean) {
+  //   this.id = id_;
+  //   this.title = title_;
+  //   this.date = date_;
+  //   this.completed = completed_;
+  // }
+}
