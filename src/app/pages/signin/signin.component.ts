95d123b85a06dd04bd57e5ac323a4726bb9e062a
Zerneax <d.chris59@gmail.com>
2020-05-17 22:42:45 +0200

    create account on firebase

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-signin',
@@ -10,7 +11,8 @@ export class SigninComponent implements OnInit {
 
   signinForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private firebaseAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
     this.initSigninForm();
@@ -21,6 +23,18 @@ export class SigninComponent implements OnInit {
       email: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
     });
+  }
+
+  createAccount() {
+    const email = this.signinForm.get('email').value;
+    const password = this.signinForm.get('password').value;
+
+    this.firebaseAuth.createUserWithEmailAndPassword(email, password).catch(
+      (error) => {
+        alert(error.code);
+        console.log(error);
+      }
+    )
   }
 
 }
